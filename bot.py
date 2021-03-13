28993b8479116511344a39247b993ec707380f44
hecronk <1damiraminov@gmail.com>
2021-03-13 23:17:54 +0500

    update bot file
    Added echo function

--- bot.py
+++ bot.py
@@ -1,5 +1,13 @@
 import telebot
 import config
+
 bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(content_types=['text'])
+def answer(message):
+    bot.send_message(message.chat.id, message.text)
+
+
 # RUN
 bot.polling(True)
