5f77165c3c585d59aa10fa091eeb422809742ed0
hecronk <1damiraminov@gmail.com>
2021-03-14 00:37:56 +0500

    update bot file
    welcome message function added

--- bot.py
+++ bot.py
@@ -4,6 +4,15 @@ import config
 bot = telebot.TeleBot(config.TOKEN)
 
 
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    sti = open('static/welcome.tgs', 'rb')
+    bot.send_sticker(message.chat.id, sti)
+
+    bot.send_message(message.chat.id,
+                     'Добро пожаловать, {0.first_name}!\nЯ - {1.first_name}, бот созданный служить тебе.'.format(message.from_user, bot.get_me()))
+    parse_mode ='html'
+
 @bot.message_handler(content_types=['text'])
 def answer(message):
     bot.send_message(message.chat.id, message.text)
