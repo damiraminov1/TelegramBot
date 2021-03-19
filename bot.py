91c16091fc44b24ea0eda30e471c3e221807a84b
hecronk <1damiraminov@gmail.com>
2021-03-19 21:38:26 +0500

    update bot file
    Added visual buttons for damir

--- bot.py
+++ bot.py
@@ -23,11 +23,33 @@ def main(message):
     if message.text =='Кубик🎲':
         bot.send_message(message.chat.id, str(random.randint(1, 6)))
     elif message.text == 'Дамир':
-        bot.send_message(message.chat.id, 'Сколько минут Дамир потратил?')
+        markup = types.InlineKeyboardMarkup(row_width=2)
+        time1 = types.InlineKeyboardButton('10 минут', callback_data='10d')
+        time2 = types.InlineKeyboardButton('20 минут', callback_data='20d')
+        time3 = types.InlineKeyboardButton('30 минут', callback_data='30d')
+        time4 = types.InlineKeyboardButton('40 минут', callback_data='40d')
+        markup.add(time1, time2, time3, time4)
+        bot.send_message(message.chat.id, 'Сколько минут Дамир потратил?', reply_markup=markup)
     elif message.text == 'Тимур':
         bot.send_message(message.chat.id, 'Сколько минут Тимур потратил?')
     elif message.text == 'Обязательства':
         bot.send_message(message.chat.id, 'Пока не умею(')
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == '10d':
+                bot.send_message(call.message.chat.id, 'Хорошо, я записал Дамиру 10 минут!')
+            elif call.data == '20d':
+                bot.send_message(call.message.chat.id, 'Хорошо, я записал Дамиру 20 минут!')
+            elif call.data == '30d':
+                bot.send_message(call.message.chat.id, 'Хорошо, я записал Дамиру 30 минут!')
+            elif call.data == '40d':
+                bot.send_message(call.message.chat.id, 'Хорошо, я записал Дамиру 40 минут!')
+    except Exception:
+        pass
+
 
 # RUN
 bot.polling(True)
